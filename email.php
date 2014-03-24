f6f783ce8e1179108b0e19b772df89f9c1b0c8af
Softdevelop <softdevelop.inc@gmail.com>
2014-03-24 21:49:59 +0700

    Update

--- email.php
+++ email.php
@@ -1,4 +1,4 @@
 <?php
 mb_language("uni");
 $body = chunk_split(base64_encode("International characters"));
-mb_send_mail("someone@example.com", "Subject", $body);
+mb_send_mail("someone@example.com", "Subject", $body);xcxz
